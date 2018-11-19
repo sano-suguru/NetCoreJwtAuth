f774ba615c2722c6f32354f19a612cf8aeff76c5
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-20 01:34:22 +0900

    Userコントローラーを実装

---
+++ NetCoreJwtAuth/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreJwtAuth.Entities;
+using NetCoreJwtAuth.Services;
+
+namespace NetCoreJwtAuth.Controllers {
+  [Authorize]
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UsersController : ControllerBase {
+    readonly IUserService userService;
+
+    public UsersController(IUserService userService) {
+      this.userService = userService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate([FromBody]AppUser user) {
+      var foundUser = userService.Authenticate(user.UserName, user.Password);
+      if (foundUser is null) {
+        return BadRequest(new { message = "ユーザー名もしくはパスワードは不正です" });
+      }
+      return Ok(foundUser);
+    }
+
+    [HttpGet]
+    public IActionResult GetAll() {
+      var users = userService.GetAll();
+      return Ok(users);
+    }
+  }
+}
