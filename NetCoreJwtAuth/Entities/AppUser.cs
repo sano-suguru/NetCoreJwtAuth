ab952f4e6501646d4960d5ac7bad85f6b111c0ac
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-20 01:17:22 +0900

    Userエンティティを追加

---
+++ NetCoreJwtAuth/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCoreJwtAuth.Entities {
+  public class AppUser {
+    public int ID { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Token { get; set; }
+  }
+}
