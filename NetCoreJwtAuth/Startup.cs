7f65ebd17d09ad585c1ec5a432615e07879c0ee5
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-20 01:37:28 +0900

    Strtup.csを更新

--- NetCoreJwtAuth/Startup.cs
+++ NetCoreJwtAuth/Startup.cs
@@ -1,28 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using NetCoreJwtAuth.Data;
+using NetCoreJwtAuth.Services;
+using System.Text;
 
 namespace NetCoreJwtAuth {
   public class Startup {
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services) {
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services) {
+      services.AddCors();
+      services.AddMvc();
+
+      services.AddDbContext<MyDbContext>(options => {
+        options.UseInMemoryDatabase(databaseName: "MyDb");
+      });
+
+      var appSettingsSection = Configuration.GetSection("AppSettings");
+      services.Configure<AppSettings>(appSettingsSection);
+
+      var appSettings = appSettingsSection.Get<AppSettings>();
+      var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+      services.AddAuthentication(options => {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      }).AddJwtBearer(options => {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(key),
+          ValidateIssuer = false,
+          ValidateAudience = false
+        };
+      });
+      services.AddScoped<IUserService, UserService>();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+      loggerFactory.AddDebug();
+
       if (env.IsDevelopment()) {
         app.UseDeveloperExceptionPage();
       }
 
-      app.Run(async (context) => {
-        await context.Response.WriteAsync("Hello World!");
+      app.UseCors(corsPolicyBuilder => {
+        corsPolicyBuilder.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader()
+          .AllowCredentials();
       });
+
+      app.UseAuthentication();
+      app.UseMvc();
     }
   }
 }
