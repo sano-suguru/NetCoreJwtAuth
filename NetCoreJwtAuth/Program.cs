b0e54d4e130d774f99a922c5d47f0ea7975d65f4
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-20 01:35:20 +0900

    フォーマットを修正

--- NetCoreJwtAuth/Program.cs
+++ NetCoreJwtAuth/Program.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace NetCoreJwtAuth {
   public class Program {
-    public static void Main(string[] args) {
+    public static void Main(string[] args) =>
       CreateWebHostBuilder(args).Build().Run();
-    }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
+      WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>();
   }
 }
