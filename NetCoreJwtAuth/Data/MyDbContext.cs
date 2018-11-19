849dd7c4c74d630f317a5b7bf08b8fb7a45519b2
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-20 01:20:09 +0900

    データーベースコンテキストを追加

---
+++ NetCoreJwtAuth/Data/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreJwtAuth.Entities;
+
+namespace CookieAuth.Models {
+  public class MyDbContext : DbContext {
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+      : base(options) { }
+
+    public DbSet<AppUser> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+      modelBuilder.Entity<AppUser>().HasData(
+        new AppUser { ID = 1, UserName = "user1", Password = "password1" },
+        new AppUser { ID = 2, UserName = "user2", Password = "password2" }
+      );
+  }
+}
